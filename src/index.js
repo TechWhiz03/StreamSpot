ca6b9adcdb885c5554136ddd70062f4188530d11
Rohila <asrohila03@gmail.com>
2023-12-17 11:13:34 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import connectDB from "./db/dbConnect.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGODB Connection Error: ${err}`);
+  });
