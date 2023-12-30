150b27116dc2df20c35077db8cf74a04eee57772
Rohila <asrohila03@gmail.com>
2023-12-30 16:01:03 +0530

    add routes for user account management controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,11 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,5 +33,16 @@ router.route("/login").post(loginUser);
 // secured routes [routes to be given to user only if logged in | login verification through "verifyJWT (auth) middleware"]
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken); // token verified in controller itself hence no "verifyJWT middleware" required
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
 export default router;
